abc97a15e0b14b033ec52ee4d435afeb37b45d5e
zalwin <inf21034@lehre.dhbw-stuttgart.de>
2024-04-10 15:24:32 +0200

    fixed type -o

--- start_application.py
+++ start_application.py
@@ -9,5 +9,5 @@ def start_application():
     os.system("systemctl start memcached")
     os.system(f"pushd /home/pi/rqlite && nohup /home/pi/rqlite/rqlited -node-id $(hostname -I | awk '{{print $1}}') "
               f"-http-addr=$(hostname -I | awk '{{print $1}}'):4001 "
-              f"-raft-addr=$$(hostname -I | awk '{{print $1}}'):4002 "
+              f"-raft-addr=$(hostname -I | awk '{{print $1}}'):4002 "
               f"-bootstrap-expect 3 -join {host1}:4002,{host2}:4002,{host3}:4002 data & disown && popd")
