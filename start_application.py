8848cfdb7df9ea6111083875989895cdc61525e5
Alwin Zomotor <inf21034@lehre.dhbw-stuttgart.de>
2024-04-13 11:54:45 +0200

    fixed rqlite start command

--- start_application.py
+++ start_application.py
@@ -1,13 +1,21 @@
 import os
 import sys
 import json
+import subprocess
 
 
 def start_application():
     config = json.load(open("config.json"))
     host1, host2, host3 = config["host1"], config["host2"], config["host3"]
     os.system("systemctl start memcached")
-    os.system(f"pushd /home/pi/rqlite && nohup /home/pi/rqlite/rqlited -node-id $(hostname -I | awk '{{print $1}}') "
-              f"-http-addr=$(hostname -I | awk '{{print $1}}'):4001 "
-              f"-raft-addr=$(hostname -I | awk '{{print $1}}'):4002 "
-              f"-bootstrap-expect 3 -join {host1}:4002,{host2}:4002,{host3}:4002 data & disown && popd")
+    own_host = subprocess.run(["hostname", "-I", "|", "awk", "'{print $1}'"], capture_output=True)
+    if own_host == host1:
+        os.system(f"bash -c \"pushd /home/pi/rqlite && nohup /home/pi/rqlite/rqlited -node-id $(hostname -I | awk '{{print $1}}') "
+                  f"-http-addr=$(hostname -I | awk '{{print $1}}'):4001 "
+                  f"-raft-addr=$(hostname -I | awk '{{print $1}}'):4002 "
+                  f"data & disown && popd\"")
+    else:
+        os.system(f"bash -c \"pushd /home/pi/rqlite && nohup /home/pi/rqlite/rqlited -node-id $(hostname -I | awk '{{print $1}}') "
+                  f"-http-addr=$(hostname -I | awk '{{print $1}}'):4001 "
+                  f"-raft-addr=$(hostname -I | awk '{{print $1}}'):4002 "
+                  f"-join {host1}:4002 data & disown && popd\"")
